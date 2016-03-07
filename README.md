42cdd4bbe87700ec4e59fe44d59f2e0b5590e8df
duaraghav8@gmail <duaraghav8@gmail.com>
2016-03-07 10:15:45 +0530

    minimax & utils.js

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ There's nothing here yet, but I wanted to initialize the repository so that cont
 - [Nicky Case: Simulating the World](http://ncase.me/simulating/)
 - [Rafael Matsunaga: Genetic Car Thingy](http://rednuht.org/genetic_cars_2/)
 - [Lee Yiyuan: 2048 Bot](http://leeyiyuan.github.io/2048ai/)
+
+
+The repository has the following structure:
+
+    There is a directory for each chapter
+      Under a chapter directory, there is one directory for each algorithm
+        Under a directory of a particular algorithm, there is a index.html which contains the visualizations for the algorithm.
+        The index.html will also execute the algorithm itself. You can view the working in the browser console.
+        There will also be a JS file in which the algorithm is implemented. This JS will be imported by index.html to execute the algorithm.
+    Apart from all chapter directories, there will be several other directories like scripts, css which contain supporting files like require.js, bootstrap, D3, etc.
---
+++ scripts/utils.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/*
+  utilities for other scripts for algorithms from the book
+  "Artificial Intelligence: A modern approach (3rd Ed.) by Russel & Norvig"
+  Author: Raghav Dua
+*/
+
+(function () {
+  var min, argmin, argmax, exportedVariables;
+
+  /*
+    min () is a python-like function which accepts a sequence followed by an evaluation function to return the minimum value in the sequence.
+    evaluation function is not necessary. If not provided, we simple return Math.min (sequence)
+  */
+  min = function (sequence, evalFunc) {
+    var minItem = null, minItemVal = Infinity;
+
+    if (typeof evalFunc == 'undefined') {
+      return (Math.min (sequence));
+    }
+    for (var i in sequence) {
+      var currentItemVal = evalFunc (sequence [i]);
+      minItem = currentItemVal < minItemVal ? sequence [i] : minItem;
+      minItemVal = Math.min (currentItemVal, minItemVal);
+    }
+    return (minItem);
+  }
+
+  argmin = function (sequence, fn) { return (min (sequence, fn)); };
+  argmax = function (sequence, fn) {
+    fn = fn || function (i) { return (i); };
+    return (min (sequence, function (i) {
+      return (-fn (i));
+    }));
+  };
+
+  /*
+    Below is the object being exposed.
+    Assuming that you call utils.js Utils upon importing it, usage is:
+
+    1. Utils.argmax ([10, 20, 30]); ==> 30
+    2. Utils.argmin (['hello', 'world', '!'], function (i) { return (i.length); });  ==> '!'
+  */
+
+  exportedVariables = {
+    argmin: argmin,
+    argmax: argmax
+  };
+
+  (function(root, factory) {
+    return (typeof define === 'function' && define.amd ? define([], factory) : null);
+  }) (this, function() {
+    return (exportedVariables);
+  });
+
+}).call (this);
