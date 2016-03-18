220937eff9c7763676b478a350af54048494ab9d
Ghost---Shadow <super.sushi269@gmail.com>
2016-03-18 22:27:49 +0530

    Added reactive cleaning robot from chapter 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+// Global imports
 import 'bootstrap/css/bootstrap.css!css';
 import './css/styles.css!css';
 import 'bootstrap';
+import './js/two.js';
+
+import './js/sh/shCore.js';
+import './js/sh/shBrushJScript.js';
+import './css/sh/shCore.css!css';
+import './css/sh/shThemeDefault.css!css';
+
+// Chapter specific imports
+import '../1-Introduction/cleaningRobot.js';
+import '../1-Introduction/canvasController.js';
