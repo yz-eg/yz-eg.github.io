a4e553330069114957b66f2f8486686c02c55c38
Rishav Agarwal <rishav.159@gmail.com>
2017-02-19 17:09:04 +0530

    Implemented breadth first search for chapter 3

--- 3-Solving-Problems-By-Searching/breadthFirstSearch.js
+++ 3-Solving-Problems-By-Searching/breadthFirstSearch.js
@@ -1,1 +1,57 @@
-// Code for Breadth First Search 
+// Code for Breadth First Search
+var get_index = function(x,y,max_row,max_col){
+  return x*max_col + y
+};
+var breadthFirstSearch = function(){
+  // Initialize the queue with the source node
+	var queue = [nodes_list[source_index]];
+  //Set interval function and assign to interval_function
+	interval_function = setInterval(function(){
+    //if queue is empty , stop the iteration
+		if(queue.length==0){
+			clearInterval(interval_function,time_interval)
+		}
+    //pop out the element from the queue
+		curr_node = queue.shift();
+    //visit that node
+		visit_node(curr_node);
+    //Check through each of the neighbors of the current node
+		for(var i=0;i<curr_node.neighbors.length;i++){
+			adj_node = nodes_list[curr_node.neighbors[i]];
+      //If node is not visited before and not blocked
+			if(!adj_node.visited && !adj_node.is_blocked){
+        //If the adjacent node is the destination, stop the iteration
+				if(adj_node.is_dest){
+					clearInterval(interval_function,time_interval);
+				}else{
+          //Push the node to the queue
+					add_to_queue(queue,adj_node)
+				}
+			}
+		}
+		two.update()
+	},time_interval);
+}
+var node = function(x,y,max_row,max_col){
+  this.x = x;
+  this.y = y;
+  this.visited = false;
+  this.is_source = false;
+  this.is_dest = false;
+  this.is_blocked = false;
+  this.neighbors = [];
+  if(this.x > 0){
+    this.neighbors.push(get_index(this.x-1,this.y,max_row,max_col))
+  };
+  if(this.y < max_col-1){
+    this.neighbors.push(get_index(this.x,this.y+1,max_row,max_col))
+  };
+  if(this.x < max_row-1){
+    this.neighbors.push(get_index(this.x+1,this.y,max_row,max_col))
+  };
+  if(this.y > 0){
+    this.neighbors.push(get_index(this.x,this.y-1,max_row,max_col))
+  };
+
+  return this;
+};
--- 3-Solving-Problems-By-Searching/c_breadthFirstSearch.js
+++ 3-Solving-Problems-By-Searching/c_breadthFirstSearch.js
@@ -1,9 +1,83 @@
+const max_row = 10;
+const max_col = 22;
+const node_size =30;
+const padding = node_size/2;
+var nodes_list = [];
+var fnodes_list = [];
+var source_index = 90;
+var dest_index = 107;
+var bfs_canvas;
+var w = 660 ,h = 300;
+var time_interval = 50;
+var interval_function;
+
+
+var visit_node = function(node){
+	node.visited = true
+	if(!curr_node.is_source && !curr_node.is_dest){
+		curr_node.f_node.fill='rgb(216, 34, 194)';
+	}
+}
+var add_to_queue = function(queue,node){
+	queue.push(node)
+	node.visited = true
+	node.f_node.fill='rgb(113, 242, 135)';
+}
+var onclick_handler = function(){
+	index = this.getAttribute('list_index')
+	curr_node = nodes_list[index]
+	if(!curr_node.is_source && !curr_node.is_dest){
+		if(curr_node.is_blocked){
+			this.setAttribute('fill','#fff');
+			curr_node.is_blocked=false;
+		}else{
+			curr_node.is_blocked=true;
+			this.setAttribute('fill','#a8a8a8');
+		};
+	};
+};
+var initialize_start_end = function(){
+	source = nodes_list[source_index];
+	destination = nodes_list[dest_index];
+	source.is_source=true
+	destination.is_dest = true
+	source.f_node.fill='green'
+	destination.f_node.fill='blue'
+};
+var init = function(){
+	clearInterval(interval_function,time_interval)
+	bfs_canvas = document.getElementById('breadthFirstSearchCanvas');
+	bfs_canvas.innerHTML = ""
+	nodes_list = []
+	two = new Two({ width: w, height: h }).appendTo(bfs_canvas);
+	for(var i=0;i<max_row;i++){
+		for(var j=0;j<max_col;j++){
+			x_offset = (j%max_col)*node_size + padding;
+			y_offset = (i%max_row)*node_size + padding;
+			var index = get_index(i,j,max_row,max_col)
+			var f_node = two.makeRectangle(x_offset,y_offset,node_size,node_size);
+			var curr_node = new node(i,j,max_row,max_col);
+			curr_node.f_node = f_node
+			two.update();
+			nodes_list.push(curr_node);
+			document.getElementById(f_node.id).setAttribute("list_index",index);
+			f_node._renderer.elem.addEventListener('mousedown',onclick_handler);
+		};
+	};
+	initialize_start_end();
+	two.update();
+}
 $(document).ready(function(){
 	$.ajax({
 		url : "breadthFirstSearch.js",
 		dataType: "text",
 		success : function (data) {
+			console.log(data);
 			$("#breadthFirstSearchCode").html(data);
 		}
 	});
+
+
+	init();
+
 });
