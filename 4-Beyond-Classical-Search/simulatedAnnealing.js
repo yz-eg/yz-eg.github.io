84c501def858ce746e820b8a789733d64b7cae22
Alex <nervgh@gmail.com>
2017-11-17 20:12:12 +0300

    Ch 4 - fix bug with random number generation
    Random numbers needed to be a half open interval a ≤ x < b instead of a closed interval a ≤ x ≤ b

--- 4-Beyond-Classical-Search/simulatedAnnealing.js
+++ 4-Beyond-Classical-Search/simulatedAnnealing.js
@@ -22,9 +22,13 @@ class SimulatedAnnealing {
       temp: temperature
     };
   }
+  /**
+   * @see https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values
+   * @returns {Number}
+   */
   getRandomState() {
     let mini = 0;
     let maxi = this.states.length;
-    return Math.floor(Math.random() * (maxi - mini + 1)) + mini;
+    return  Math.floor(Math.random() * (maxi - mini)) + mini;
   }
 }
